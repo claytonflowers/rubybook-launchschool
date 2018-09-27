83dfc485342ceed1cb4b9bfc09ae1d9afb2075a9
Clayton Flowers <flowers.clayton@gmail.com>
2018-09-27 16:10:51 -0400

    Add files via upload
    Added Methods Chapter

---
+++ methods.rb
@@ -0,0 +1,26 @@
+# Methods Excersizes
+
+# 1. 
+def greeting(name)
+  puts 'Hi, ' + name + "."
+end
+
+greeting("Clayton")
+
+# 2. 
+x = 2
+puts x = 2
+p name = "Joe"
+four = "four"
+print something = "nothing"
+
+# 3. 
+def multiply(num_1, num_2)
+  puts num_1 * num_2
+end
+
+# 4. 
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
